88597713162762634556b5344538adeefffc503e
JC-kip <jjcasasm16@gmail.com>
2024-08-27 00:48:33 +0000

    PartTwo

--- src/app.js
+++ src/app.js
@@ -5,25 +5,29 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let names = [
-  "Last Whisper",
-  "Krakosh The Mighty",
-  "Sea Scent",
-  "Gyadar of Nature",
-  "Abyss The Great"
-];
-let types = ["Knight", "Archer", "Beast", "Fairy", "Dragon"];
-let natures = ["Water", "Earth", "Air", "Fire", "Spirit"];
+window.onload = function() {
+  document.querySelector("#female").addEventListener("click", function() {
+    generateExcuse("female");
+  });
 
-function generateExcuse() {
-  let randomName = names[Math.floor(Math.random() * names.length)];
-  let randomType = types[Math.floor(Math.random() * types.length)];
-  let randomNature = natures[Math.floor(Math.random() * natures.length)];
 
-  let excuse = `Your Helping Partner Is: ${randomName}, The ${randomNature} ${randomType}.`;
-  return excuse;
-}
-function displayExcuse() {
-  let excuse = generateExcuse();
-  document.getElementById("excuse").innerText = excuse;
-}
+
+  let name = [
+    "Last Whisper",
+    "Krakosh The Mighty",
+    "Sea Scent",
+    "Gyadar of Nature",
+    "Abyss The Great"
+  ];
+  let type = ["Knight", "Archer", "Beast", "Fairy", "Dragon"];
+  let nature = ["Water", "Earth", "Air", "Fire", "Spirit"];
+
+  function generateExcuse() {
+    let randomName = name[Math.floor(Math.random() * name.length)];
+    let randomType = type[Math.floor(Math.random() * type.length)];
+    let randomNature = nature[Math.floor(Math.random() * nature.length)];
+
+    let excuse = "Your Helping Partner Is:" + name[randomName] + ", The" + nature[randomNature] + type[randomType] + ".";
+    document.querySelector(".characterName").innerHTML = excuse;
+  }
+};
