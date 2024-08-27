05d1aaaf0d75d40ff6bef79fde587d363c43daae
JC-kip <jjcasasm16@gmail.com>
2024-08-27 00:03:10 +0000

    Part1

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+let names = [
+  "Last Whisper",
+  "Krakosh The Mighty",
+  "Sea Scent",
+  "Gyadar of Nature",
+  "Abyss The Great"
+];
+let types = ["Knight", "Archer", "Beast", "Fairy", "Dragon"];
+let natures = ["Water", "Earth", "Air", "Fire", "Spirit"];
+
+function generateExcuse() {
+  let randomName = names[Math.floor(Math.random() * names.length)];
+  let randomType = types[Math.floor(Math.random() * types.length)];
+  let randomNature = natures[Math.floor(Math.random() * natures.length)];
+
+  let excuse = `Your Helping Partner Is: ${randomName}, The ${randomNature} ${randomType}.`;
+  return excuse;
+}
+function displayExcuse() {
+  let excuse = generateExcuse();
+  document.getElementById("excuse").innerText = excuse;
+}
