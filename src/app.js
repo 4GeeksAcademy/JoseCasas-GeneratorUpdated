4f44b96d47992e8e6d77f695aaf459675378a9b9
JC-kip <jjcasasm16@gmail.com>
2024-09-08 22:14:49 +0000

    Project Rebuilt

--- src/app.js
+++ src/app.js
@@ -5,37 +5,59 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  document.querySelector("#helper").addEventListener("click", function() {
-    generateExcuse("helper");
-  });
+// window.onload = function() {
+//   document.querySelector("#helper").addEventListener("click", function() {
+//     generateExcuse("helper");
+//   });
 
-  let name = [
+//   let name = [
+//     "Last Whisper",
+//     "Krakosh The Mighty",
+//     "Sea Scent",
+//     "Gyadar of Nature",
+//     "Abyss The Great"
+//   ];
+//   let type = ["Knight", "Archer", "Beast", "Fairy", "Dragon"];
+//   let nature = ["Water", "Earth", "Air", "Fire", "Spirit"];
+
+//   function generateExcuse() {
+//     let randomName = name[Math.floor(Math.random() * name.length)];
+//     let randomType = type[Math.floor(Math.random() * type.length)];
+//     let randomNature = nature[Math.floor(Math.random() * nature.length)];
+
+//     let excuse =
+//       "Your Helping Partner Is:" +
+//       " " +
+//       [randomName] +
+//       " " +
+//       ", The" +
+//       " " +
+//       [randomNature] +
+//       " " +
+//       [randomType] +
+//       ".";
+//     document.querySelector(".characterName").innerHTML = excuse;
+//   }
+// };
+window.onload = () => {
+  document.querySelector("#helper").addEventListener("click", generateExcuse);
+
+  const name = [
     "Last Whisper",
     "Krakosh The Mighty",
     "Sea Scent",
     "Gyadar of Nature",
     "Abyss The Great"
   ];
-  let type = ["Knight", "Archer", "Beast", "Fairy", "Dragon"];
-  let nature = ["Water", "Earth", "Air", "Fire", "Spirit"];
+  const type = ["Knight", "Archer", "Beast", "Fairy", "Dragon"];
+  const nature = ["Water", "Earth", "Air", "Fire", "Spirit"];
 
   function generateExcuse() {
-    let randomName = name[Math.floor(Math.random() * name.length)];
-    let randomType = type[Math.floor(Math.random() * type.length)];
-    let randomNature = nature[Math.floor(Math.random() * nature.length)];
+    const randomName = name[Math.floor(Math.random() * name.length)];
+    const randomType = type[Math.floor(Math.random() * type.length)];
+    const randomNature = nature[Math.floor(Math.random() * nature.length)];
 
-    let excuse =
-      "Your Helping Partner Is:" +
-      " " +
-      [randomName] +
-      " " +
-      ", The" +
-      " " +
-      [randomNature] +
-      " " +
-      [randomType] +
-      ".";
+    const excuse = `Your Helping Partner Is: ${randomName}, The ${randomNature} ${randomType}.`;
     document.querySelector(".characterName").innerHTML = excuse;
   }
 };
